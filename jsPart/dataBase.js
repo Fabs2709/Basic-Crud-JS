614b77e0ae9f171eb6d64a23859527ed8a09f1db
Fabs2709 <113150219+Fabs2709@users.noreply.github.com>
2023-03-03 18:50:53 -0300

    Update dataBase.js

--- jsPart/dataBase.js
+++ jsPart/dataBase.js
@@ -5,29 +5,24 @@ const sequence = {
 
 const produtos = {}
 
-//Adicionar
 function salvarProduto(produto){
-    if(!produto.id) produto.id = sequence.id //Se o id do produto não estiver setado, ele vai receber o id livre da sequencia
-    produtos[produto.id] = produto //Caso o produto já estiver setado ele substitui pela versão mais nova, caso contrário ele adiciona o novo elemento dentro de produtos
+    if(!produto.id) produto.id = sequence.id
+    produtos[produto.id] = produto 
     return produto
 }
 
-//Retornar produto especifico
 function getProduto(id){
     return produtos[id] || {}
 }
 
-//Retornar todos os produtos
 function getProdutos(){
     return Object.values(produtos)
 }
 
-//Excluir
 function excluirProduto(id){
-    const produto = produtos[id] //Guardar o produto excluido em um constante local
-    delete produtos[id] //Apagar o produto escolhido
-    return produto //Retornar o produto que havia sido excluido (pra exibir na tela o que foi excluido, etc)
-}
+    const produto = produtos[id] 
+    delete produtos[id] 
+    return produto 
 
 
 module.exports = {salvarProduto, getProduto, getProdutos, excluirProduto}
