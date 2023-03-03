977e41cd4dc7110fc9fbfe541ccb392b1e7bf49d
Fabs2709 <113150219+Fabs2709@users.noreply.github.com>
2023-03-03 18:52:35 -0300

    Update server.js

--- jsPart/server.js
+++ jsPart/server.js
@@ -1,51 +1,42 @@
-const porta = 2709 //A porta é a forma de selecionar qual processo vai atender aquela requisição / Porta padrão é a 80
+const porta = 2709
 
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const bancoDeDados = require('./dataBase')
 
-app.use(bodyParser.urlencoded({extended: true})) //Se os dados estiverem sendo enviados no padrão urlencoded, ele vai transformar as infos em um objeto para poderem ser acessadas de uma forma mais tranquila
+app.use(bodyParser.urlencoded({extended: true}))
 
-//Exibir
-app.get('/produtos', (req, res, next) => { //get = forma de requisição 
-    res.send(bancoDeDados.getProdutos()) //Envia todos os produtos cadastrados (res.send = enviar resposta)
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
 })
 
-app.get('/produtos/:id', (req, res, next) => { //Tem que receber o id também (isso que o /:id indica)
-    res.send(bancoDeDados.getProduto(req.params.id)) //Exibir um produto especifico localizado por ID (req.params = parametros passados na url (eles vem do request))
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-//Adicionar
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.nome, //Pegar o nome a partir do corpo da composição
-        preco: req.body.preco //Pegar o preco a partir do corpo da composição
+        nome: req.body.nome,
+        preco: req.body.preco
     })
-    res.send(produto) //retorna um JSON
+    res.send(produto)
 })
 
-//Alterar
 app.put('/produtos/:id', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         id: req.params.id, 
-        nome: req.body.nome, //Alterar o nome a partir do corpo da composição
-        preco: req.body.preco //Alterar o preco a partir do corpo da composição
+        nome: req.body.nome,
+        preco: req.body.preco
     })
-    res.send(produto) //retorna um JSON
+    res.send(produto)
 })
 
-//Excluir
 app.delete('/produtos/:id', (req, res, next) => {
     const produto = bancoDeDados.excluirProduto(req.params.id)
-    res.send(produto) //retorna um JSON
+    res.send(produto)
 })
 
-app.listen(porta, () => { //Porta que vai executar o app
+app.listen(porta, () => {
     console.log(`Servidor esta executando na porta ${porta}`)
 })
-
-/*Sobre o Postman:
-- Post: Posso inserir as coisas
-- Get: Exibir as coisas (Para conseguir ver uma coisa especifica, colocar: /(identificador selecionado), EX: http://localhost:2709/produtos/2)
-- Put: Permite alterar as infos (Colocar o identificador do item que deseja alterar e mudar no mesmo local que add)*/
